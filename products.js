63c5498b271af91bc71ea350927a8ee4f79dea5e
luisaromero <luisa.marcela.romero@gmail.com>
2026-08-02 05:31:19 -0400

    add id to products

--- products.js
+++ products.js
@@ -1,53 +1,62 @@
 const products = [
     {
+        id: 1,
         nombre: "Conjunto Padel Mujer Naranjo",
         precio: 45000,
         disponible: true,
         imagen: "/img/products/orange-dress.png"
     },
     {
+        id: 2,
         nombre: "Conjunto Padel Mujer Negro",
         precio: 45000,
         disponible: true,
         imagen: "/img/products/black-dress.png"
     },
     {
+        id: 3,
         nombre: "Raqueta Rosada",
         precio: 46000,
         disponible: true,
         imagen: "/img/products/pink-racket.png"
     },
     {
+        id: 4,
         nombre: "Raqueta Multicolor",
         precio: 40000,
         disponible: true,
         imagen: "/img/products/multicolor-racket.png"
     },
     {
+        id: 5,
         nombre: "Polera Polo Rosada",
         precio: 20000,
         disponible: false,
         imagen: "/img/products/pink-shirt.png"
     },
     {
+        id: 6,
         nombre: "Raqueta de Padel Pro Aquamarine",
         precio: 89000,
         disponible: true,
         imagen: "/img/products/white-racket.png"
     },
     {
+        id: 7,
         nombre: "Falda Deportiva Azul",
         precio: 19000,
         disponible: false,
         imagen: "/img/products/blue-skirt.png"
     },
     {
+        id: 8,
         nombre: "Raqueta de Padel Iniciación",
         precio: 55000,
         disponible: true,
         imagen: "/img/products/black-racket.png"
     },
     {
+        id: 9,
         nombre: "Conjunto Padel Mujer Rosado",
         precio: 62000,
         disponible: true,
@@ -55,18 +64,21 @@ const products = [
     },
 
     {
+        id: 10,
         nombre: "Botella de Agua Rosada 750ml",
         precio: 9000,
         disponible: true,
         imagen: "/img/products/pink-water-bottle.png"
     },
     {
+        id: 11,
         nombre: "Jockey Deportiva azul",
         precio: 15000,
         disponible: true,
         imagen: "/img/products/blue-jockey.png"
     },
     {
+        id: 12,
         nombre: "Tubo de Pelotas Padel x3",
         precio: 7500,
         disponible: true,
